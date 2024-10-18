ccaa038fb81427e5d59510b2ca628e303fae020b
vetvaag <117277106+vetvaag@users.noreply.github.com>
2024-10-18 11:13:40 +0200

    Add project files.

---
+++ 4opeenrij/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _4opeenrij
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.turnLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Location = new System.Drawing.Point(28, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(421, 375);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(522, 89);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(189, 78);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Herstarten";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Fredoka One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(505, 12);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(216, 32);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "4 Op Een Rij - Pj";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // turnLabel
+            // 
+            this.turnLabel.AutoSize = true;
+            this.turnLabel.Font = new System.Drawing.Font("Fredoka One", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turnLabel.Location = new System.Drawing.Point(584, 45);
+            this.turnLabel.Name = "turnLabel";
+            this.turnLabel.Size = new System.Drawing.Size(70, 25);
+            this.turnLabel.TabIndex = 3;
+            this.turnLabel.Text = "Beurt";
+            this.turnLabel.Click += new System.EventHandler(this.turnLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.turnLabel);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label turnLabel;
+    }
+}
+
---
+++ 4opeenrij/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _4opeenrij
+{
+    public partial class Form1 : Form
+    {
+        private const int Rows = 6;
+        private const int Cols = 7;
+        private const int CellSize = 50;
+        private char[,] board = new char[Rows, Cols];
+        private bool isPlayerOneTurn = true;
+        private bool gameOver = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "4 op een rij";
+            gamePanel.Paint += GamePanel_Paint;
+            gamePanel.MouseClick += GamePanel_MouseClick;
+            resetButton.Click += ResetButton_Click;
+
+            // Set initial turn label
+            turnLabel.Text = "Speler 1's beurt"; // Player 1's turn
+            ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Cols; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+            isPlayerOneTurn = true;
+            gameOver = false;
+            turnLabel.Text = "Speler 1's beurt"; // Reset to Player 1's turn
+            gamePanel.Invalidate();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void GamePanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Cols; col++)
+                {
+                    DrawCell(g, row, col);
+                }
+            }
+        }
+
+        private void DrawCell(Graphics g, int row, int col)
+        {
+            int x = col * CellSize;
+            int y = row * CellSize;
+            g.DrawRectangle(Pens.Black, x, y, CellSize, CellSize);
+
+            if (board[row, col] == 'O')
+            {
+                g.DrawEllipse(Pens.Blue, x + 5, y + 5, CellSize - 10, CellSize - 10);
+            }
+            else if (board[row, col] == 'X')
+            {
+                g.DrawLine(Pens.Red, x + 5, y + 5, x + CellSize - 5, y + CellSize - 5);
+                g.DrawLine(Pens.Red, x + CellSize - 5, y + 5, x + 5, y + CellSize - 5);
+            }
+        }
+
+        private void GamePanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (gameOver)
+                return;
+
+            int col = e.X / CellSize;
+
+            if (col >= 0 && col < Cols)
+            {
+                for (int row = Rows - 1; row >= 0; row--)
+                {
+                    if (board[row, col] == ' ')
+                    {
+                        board[row, col] = isPlayerOneTurn ? 'O' : 'X';
+                        gamePanel.Invalidate();
+
+                        if (CheckForWinner(row, col))
+                        {
+                            gameOver = true;
+                            MessageBox.Show($"Speler {(isPlayerOneTurn ? 1 : 2)} wint!");
+                            return;
+                        }
+                        else if (IsBoardFull())
+                        {
+                            gameOver = true;
+                            MessageBox.Show("Gelijkspel");
+                            return;
+                        }
+
+                        isPlayerOneTurn = !isPlayerOneTurn;
+
+                        // Update the turn label
+                        turnLabel.Text = isPlayerOneTurn ? "Speler 1's beurt" : "Speler 2's beurt";
+                        break;
+                    }
+                }
+            }
+        }
+
+        private bool CheckForWinner(int row, int col)
+        {
+            char player = board[row, col];
+            return CheckDirection(row, col, 1, 0, player) ||
+                   CheckDirection(row, col, 0, 1, player) ||
+                   CheckDirection(row, col, 1, 1, player) ||
+                   CheckDirection(row, col, 1, -1, player);
+        }
+
+        private bool CheckDirection(int row, int col, int dRow, int dCol, char player)
+        {
+            int count = 1;
+            count += CountInDirection(row, col, dRow, dCol, player);
+            count += CountInDirection(row, col, -dRow, -dCol, player);
+            return count >= 4;
+        }
+
+        private int CountInDirection(int row, int col, int dRow, int dCol, char player)
+        {
+            int r = row + dRow;
+            int c = col + dCol;
+            int count = 0;
+            while (r >= 0 && r < Rows && c >= 0 && c < Cols && board[r, c] == player)
+            {
+                count++;
+                r += dRow;
+                c += dCol;
+            }
+            return count;
+        }
+
+        private bool IsBoardFull()
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Cols; col++)
+                {
+                    if (board[row, col] == ' ')
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void turnLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
